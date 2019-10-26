aee4be91af9a5b12474af15a5759860fba5802d7
ChrisYeomans <chrisj.yeomans@gmail.com>
2019-10-26 21:26:41 +0100

    Added info Pages

--- app.py
+++ app.py
@@ -5,17 +5,22 @@ from station_info import StationInfo
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+st = StationInfo()
 
 def main():
+
 	Bootstrap(app)
 	app.run()
 
 @app.route('/')
 def index():
-	st = StationInfo("Dublin Connolly")
-	info = st.info_lst
+	info = st.get_info_lst("Dublin Connolly")
 	return render_template('index.html', info=info)
 
+@app.route('/info/<st_name>')
+def info(st_name):
+	info = st.get_info_lst(st_name)
+	return render_template('info.html', info=info)
 
 if __name__ == "__main__":
 	main()
--- station_info.py
+++ station_info.py
@@ -2,21 +2,22 @@ import requests
 import xml.etree.ElementTree as ET
 
 class StationInfo:
-    info_lst = []
-    station_code = ""
-    station_name = ""
-
-    def __init__(self, station_name: str):
-        self.station_name = station_name
-        self.station_code = self.station_name_to_code(station_name)
-        info_xml = ET.fromstring(requests.get(f"http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML?StationCode={self.station_code}").text)
+    def __init__(self):
+        pass 
         
+    def get_info_lst(self, station_name):
+        station_code = self.station_name_to_code(station_name)
+        info_xml = ET.fromstring(requests.get(f"http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML?StationCode={station_code}").text)
+        info_lst = []
+
         for i in info_xml:
             tmp_dict = {}
             for j in i:
                 tmp_dict[j.tag.split("{http://api.irishrail.ie/realtime/}")[1]] = j.text
-            if tmp_dict['Destination'] != self.station_name:
-                self.info_lst.append(tmp_dict)
+            #if tmp_dict['Destination'] != self.station_name:
+            info_lst.append(tmp_dict)
+        
+        return info_lst
     
     def station_name_to_code(self, station_name: str):
         #TODO: Make this cached
@@ -28,6 +29,6 @@ class StationInfo:
         return out_dict[station_name.upper()]
 
 if __name__ == "__main__":
-    st = StationInfo("Dublin Connolly")
-    #print(st.station_code)
+    st = StationInfo("Cork")
+    print(st.station_code)
 
