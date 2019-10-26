65bfd71ed2f510dd887984bb605af7ff205d3e34
ChrisYeomans <chrisj.yeomans@gmail.com>
2019-10-26 16:45:42 +0100

    class initializer now collects all train info into a list of dicts

--- station_info.py
+++ station_info.py
@@ -2,22 +2,37 @@ import requests
 import xml.etree.ElementTree as ET
 
 class StationInfo:
+    info_lst = []
+    station_code = ""
+    station_name = ""
+
     def __init__(self, station_name: str):
-        station_code = self.station_name_to_code(station_name)
-        info_xml = requests.get(f"http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML?StationCode={station_code}")
-        print(info_xml.text)
+        self.station_name = station_name
+        self.station_code = self.station_name_to_code(station_name)
+        info_xml = ET.fromstring(requests.get(f"http://api.irishrail.ie/realtime/realtime.asmx/getStationDataByCodeXML?StationCode={self.station_code}").text)
+        
+        for i in info_xml:
+            tmp_dict = {}
+            for j in i:
+                tmp_dict[j.tag.split("{http://api.irishrail.ie/realtime/}")[1]] = j.text
+            if tmp_dict['Destination'] != self.station_name:
+                self.info_lst.append(tmp_dict)
+    
+        for p in self.info_lst:
+            print(p['Traincode'])
+
+        
     
     def station_name_to_code(self, station_name: str):
-        #TODO: Make this cached later
+        #TODO: Make this cached
         out_dict = {}
         stations_xml = requests.get("http://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML").text
         stations = ET.fromstring(stations_xml)
         for station in stations:
             out_dict[station[0].text.upper()] = station[4].text
-            if station[1].text:
-                out_dict[station[1].text.upper()] = station[4].text
         return out_dict[station_name.upper()]
 
 if __name__ == "__main__":
-    st = StationInfo("Connolly")
+    st = StationInfo("Dublin Connolly")
+    #print(st.station_code)
 
